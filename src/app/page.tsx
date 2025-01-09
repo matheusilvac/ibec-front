b6ac0d41062885cef6c669e7a4f1716739ad72b2
matheuscs0 <dev.matheuscs@gmail.com>
2025-01-09 13:27:56 -0300

    update mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,26 @@ export default function Home() {
   return (
     <div className="w-full h-full flex flex-col pt-32 pb-10 text-[#0A1A2D]">
       <div className="w-full h-full flex flex-col justify-center items-center mb-20 px-5 md:px-20">
-        <div className="w-full h-full flex flex-col sm:flex-row justify-center md:justify-around items-center mb-10 px-5 md:px-20">
+        <div className="w-full h-full flex flex-col sm:flex-row justify-center md:justify-around items-center mb-14 px-5 md:px-10">
           <div className="w-full h-full sm:w-1/2 justify-center items-center flex text-start mb-5 sm:mb-0">
-            <h1 className="text-7xl text-left uppercase font-semibold">
-              Instítuto <br /> Bíblico de <br /> Educação <br /> Cristã
+            <h1 className="text-3xl md:text-7xl text-center sm:text-left uppercase font-semibold">
+              Instítuto <br />Bíblico de <br /> Educação <br /> Cristã
             </h1>
           </div>
           <div className="w-full h-full sm:w-1/2 flex justify-center items-center">
-            <p className="text-center sm:text-left text-xl">
-              <strong className="text-3xl text-[#e6a44e]">
+            <p className="text-center sm:text-left text-lg sm:text-xl">
+              <strong className="text-xl sm:text-3xl bg-[#e6a44e]">
                 Mateus 7:17-20
               </strong>
               , que enfatiza que uma{" "}
-              <strong className="text-[#e6a44e]">“árvore boa”</strong> é
+              <strong className="bg-[#e6a44e]">“árvore boa”</strong> é
               reconhecida pelos frutos que produz. Esse conceito simboliza
               diretamente o objetivo do instituto: formar líderes comprometidos
               com a verdadeira palavra de Deus e que sejam capazes de gerar bons
               frutos no seu ministério.
               <br />
               <br />O conceito de{" "}
-              <strong className="text-[#e6a44e]">“bons frutos”</strong> reforça
+              <strong className="bg-[#e6a44e]">“bons frutos”</strong> reforça
               a missão do IBEC de educar as pessoas sobre a verdadeira palavra
               de Deus e corrigir as deficiências na formação ministerial. Cada
               fruto gerado representa um líder bem preparado e uma comunidade
@@ -35,7 +35,7 @@ export default function Home() {
           </div>
         </div>
         <div className="w-full flex justify-center items-center h-full">
-          <img src="/arvores.png" alt="" className="w-1/2 h-auto flex" />
+          <img src="/arvores.png" alt="" className="w-full sm:w-1/2 h-auto flex" />
         </div>
       </div>
       <div className="w-full flex flex-wrap justify-center items-center gap-8">
