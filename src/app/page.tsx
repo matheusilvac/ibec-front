836dcfef121c1a2d8a23417f015b9925d89227ee
matheuscs0 <dev.matheuscs@gmail.com>
2025-01-09 23:44:13 -0300

    att

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import { ElementoWidget } from "@/components/elementoWidget";
-import { Loading } from "@/components/loading";
-import Image from "next/image";
-import { FaBible, FaChurch, FaCross } from "react-icons/fa";
+import { FaBible, FaBook, FaChurch, FaCross } from "react-icons/fa";
+import { PiCertificateFill } from "react-icons/pi";
 
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col pt-32 pb-10 text-[#0A1A2D]">
-      <div className="w-full h-full flex flex-col justify-center items-center mb-20 px-5 md:px-20">
+      <div className="w-full h-full flex flex-col justify-center items-center mb-5 sm:mb-20 px-5 md:px-20">
         <div className="w-full h-full flex flex-col sm:flex-row justify-center md:justify-around items-center mb-10 sm:mb-14 px-0 sm:px-5">
-          <div className="w-full h-full sm:w-1/2 justify-center items-center flex text-start mb-5 sm:mb-0">
-            <h1 className="text-4xl md:text-7xl text-center sm:text-left uppercase font-semibold ">
-              Instítuto <span>Bíblico</span> de <br /> Educação Cristã
+          <div className="w-full h-full sm:w-2/3 justify-center items-center flex text-start mb-5 sm:mb-0">
+            <h1 className="text-4xl md:text-6xl text-center sm:text-left uppercase font-semibold ">
+              Instítuto Bíblico<br /> de <br /> Educação Cristã
             </h1>
           </div>
           <div className="w-full h-full sm:w-1/2 flex justify-center items-center">
@@ -42,28 +41,24 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="w-full flex flex-col justify-center items-center h-full mb-10 px-5 md:px-20">
-        <div className="w-full flex flex-col justify-center items-center gap-8 mb-5">
-          <h1 className="text-3xl font-semibold">Proposta</h1>
-          <p className="w-full flex justify-center items-center sm:w-2/3 text-left text-xl">
-            O projeto do IBEC – Instituto Bíblico de Educação Cristã – foi
-            idealizado há cerca de quatro anos, com uma visão clara e um
-            propósito profundo.
-            <br /> Embora haja uma variedade de cursos teológicos disponíveis no
-            mercado, os fundadores do IBEC perceberam que muitos desses
-            seminários se concentram exclusivamente na transmissão de
-            conhecimento acadêmico, sem considerar a formação para o exercício
-            do ministério. <br />A proposta do IBEC é ir além do ensino
-            teológico tradicional: o objetivo é oferecer uma formação que ira
-            forjar, de maneira sólida, homens piedosos, que temem a Deus e
-            buscam viver sua fé de forma autêntica e prática.
-          </p>
-        </div>
-        <div className="w-full flex flex-col sm:flex-row justify-center items-center sm:justify-around">
-          <div className="w-full sm:w-1/2 flex flex-col justify-center items-center mb-5 sm:mb-0">
-            <h1 className="text-3xl font-semibold">Missão</h1>
-            <p className="w-full flex justify-center items-center sm:w-2/3 text-left text-xl">
-              A missão do IBEC é formar homens que, ao se aprofundarem na
+        <div className="w-full h-full flex flex-col sm:flex-row justify-center sm:justify-around items-center mb-10 px-5 md:px-20">
+          <div className="w-full flex flex-col justify-center items-center gap-3 mb-5">
+            <h1 className="text-3xl font-semibold uppercase justify-center sm:justify-start flex w-full">
+              Proposta
+            </h1>
+            <p className="flex justify-center items-center text-center sm:text-left text-lg">
+              O projeto do IBEC – Instituto Bíblico de Educação Cristã – foi
+              idealizado há cerca de quatro anos, com uma visão clara e um
+              propósito profundo.
+              <br /> Embora haja uma variedade de cursos teológicos disponíveis
+              no mercado, os fundadores do IBEC perceberam que muitos desses
+              seminários se concentram exclusivamente na transmissão de
+              conhecimento acadêmico, sem considerar a formação para o exercício
+              do ministério. <br />A proposta do IBEC é ir além do ensino
+              teológico tradicional: o objetivo é oferecer uma formação que ira
+              forjar, de maneira sólida, homens piedosos, que temem a Deus e
+              buscam viver sua fé de forma autêntica e prática.
+              <br />A missão do IBEC é formar homens que, ao se aprofundarem na
               teologia, reconheçam sua total dependência de Deus e, a partir
               disso, sejam capacitados a servir à Igreja de Cristo com eficácia
               e integridade. A teologia ensinada no IBEC visa à construção de um
@@ -71,19 +66,41 @@ export default function Home() {
               ações e atitudes cotidianas.
             </p>
           </div>
-          <div className="w-full sm:w-1/2 flex flex-col justify-center items-center">
-            <h1 className="text-3xl font-semibold">Propósito</h1>
-            <p className="w-full flex justify-center items-center sm:w-2/3 text-left text-xl">
-              Após quase quatro anos de planejamento e preparo, o projeto
-              finalmente foi concretizado. Em setembro de 2024, o IBEC foi
-              oficialmente iniciada, marcando o começo de uma jornada que busca
-              impactar vidas de maneira profunda. A proposta do IBEC é,
-              portanto, ser um verdadeiro instrumento de transformação, formando
-              líderes cristãos preparados para fazer diferença na Igreja e na
-              sociedade por meio do ensino da Palavra de Deus e de um
-              compromisso genuíno com a fé cristã.
-            </p>
-          </div>
+          <div className="w-full flex justify-center items-center">
+            <div className="w-3/4 h-auto flex flex-wrap justify-center items-center gap-8 bg-[#0A1A2D] rounded-xl p-5">
+              <section className="max-w-[200px] h-auto flex justify-around items-center gap-3">
+                <div className="w-20 h-20 justify-center rounded-xl bg-[#e6a44e] flex shadow-md shadow-black/25">
+                  <FaChurch className="w-2/3 h-auto" />
+                </div>
+                <p className="text-white flex text-left w-24 text-sm">
+                  Curso aceito em todas as igrejas evangélicas
+                </p>
+              </section>
+              <section className="max-w-[200px] h-auto flex justify-around items-center gap-3">
+                <div className="w-20 h-20 justify-center rounded-xl bg-[#e6a44e] flex shadow-md shadow-black/25">
+                  <PiCertificateFill className="w-2/3 h-auto" />
+                </div>
+                <p className="text-white flex text-left w-24 text-sm">
+                  Certificado com carga horária de 2.400h aulas
+                </p>
+              </section>
+              <section className="max-w-[200px] h-auto flex justify-around items-center gap-3">
+                <div className="w-20 h-20 justify-center rounded-xl bg-[#e6a44e] flex shadow-md shadow-black/25">
+                  <FaBook className="w-2/3 h-auto" />
+                </div>
+                <p className="text-white flex text-left w-24 text-sm">
+                  Grade de aulas completa, novo e velho testamento
+                </p>
+              </section>
+              <section className="max-w-[200px] h-auto flex justify-around items-center gap-3">
+                <div className="w-20 h-20 justify-center rounded-xl bg-[#e6a44e] flex shadow-md shadow-black/25">
+                  <FaBook className="w-2/3 h-auto" />
+                </div>
+                <p className="text-white flex text-left w-24 text-sm">
+                  Livraria completa
+                </p>
+              </section>
+            </div>
         </div>
       </div>
       <div className="w-full flex flex-wrap justify-center items-center gap-8">
